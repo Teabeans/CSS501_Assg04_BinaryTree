03a5bc4836278b98856f10020551ca608856412b
Tim Lum <twhlum@gmail.com>
2017-11-25 02:45:48 -0800

    Update NodalBST.cpp

--- NodalBST.cpp
+++ NodalBST.cpp
@@ -4,50 +4,10 @@ using namespace std;
 template <class typeT>
 typeT objData;
 
+// Default constructor
 template <class typeT>
-typeT variable2;
-
-//template <class typeT>
-//BSTNode* headPtr;
-
-
-
-// X------------------X
-// |                  |
-// |    STRUCTURES    |
-// |                  |
-// X------------------X
-
-template <class typeT>
-struct BSTNode {
-   typeT nodeData;
-   BSTNode<typeT>* leftPtr;
-   BSTNode<typeT>* rightPtr;
-   bool isDeleted;
-
-   // X---- CONSTRUCTORS ----X
-
-
-   BSTNode(typeT someData) {
-      // NOTE: Never initialize a generic type variable in a constructor
-      nodeData = someData;
-      isDeleted = true;
-   }
-}; // End struct
-
-// X---------------------X
-// |                     |
-// |    PUBLIC FIELDS    |
-// |                     |
-// X---------------------X
-template <class typeT>
-BSTNode<typeT>* headPtr;
-
-// X----------------------X
-// |                      |
-// |    PUBLIC METHODS    |
-// |                      |
-// X----------------------X
+NodalBST<typeT>::NodalBST() {
+}
 
 // Getter
 template <class typeT>
@@ -59,16 +19,4 @@ typeT NodalBST<typeT>::GetValue() {
 template <class typeT>
 void NodalBST<typeT>::SetValue( typeT someValue ) {
    objData = someValue; // Set the received value to the generic datatype object m_obj
-}
-
-// X--------------------------------X
-// |                                |
-// |    CONSTRUCTORS/DESTRUCTORS    |
-// |                                |
-// X--------------------------------X
-
-// Default constructor
-template <class typeT> NodalBST<typeT>::NodalBST() {
-   cout << "Default constructor for NodalBST activated." << endl;
-   headPtr = BSTNode<typeT>(typeT someData); // TODO: Add 'new'?
 }
